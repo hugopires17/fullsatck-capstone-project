4fc725ce6c11e02b70307df8a6bc694d62b8fe19
hugopires17 <hugopprocopio@hotmail.com>
2025-06-10 16:10:30 -0300

    Refactor app.js to improve MongoDB connection handling and update route imports. Added authentication routes and clarified comments.

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,21 +7,24 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+
 const app = express();
 app.use("*",cors());
 const port = 3060;
 
-// Connect to MongoDB
+// Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-}).catch((e) => console.error('Failed to connect to DB', e));
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
+
 
 app.use(express.json());
 
-// Route imports
+// Route files
 const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes = require('./routes/searchRoutes'); // Search API Task 1
-
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -29,7 +32,8 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
-app.use('/api/search', searchRoutes); // Search API Task 2
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
